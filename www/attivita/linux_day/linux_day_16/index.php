7e53ec7f996f174cba2c0ba58524b3b203ca27cd
Andrea Gelmini <andrea.gelmini@gelma.net>
2011-07-20 17:08:14 +0200

    Aggiunto scheletro per Linux Day dell'anno prossimo

---
+++ www/attivita/linux_day/linux_day_16/index.php
@@ -0,0 +1,57 @@
+<?php   $titolo='LugBS &raquo; Attivit&agrave; &raquo; Linux Day &raquo; Quindicesima edizione';
+        include($_SERVER["DOCUMENT_ROOT"].'/includes/header.inc'); 
+
+function talk($ora,$email,$oratore,$titolo){
+    print "<dt class=\"programma\">$ora <span class=\"relatore\"><a href=\"mailto:$email\">$oratore</a></span></dt>
+           <dd class=\"programma\">$titolo</dd>";
+}
+?>
+
+<H3 align="center">Quindicesimo Linux Day: Linux Day Nazionale</H3>
+
+<dt>Data e Luogo</dt>
+<dd>
+<p>Nell'ambito della <a href="http://www.linuxday.it/">Giornata Nazionale di Linux e del Software Libero</a>, promossa da
+<a href="http://www.linux.it/">ILS</a>, il LugBS organizza il suo quindicesimo Linux Day. La manifestazione ha luogo
+Sabato 22 Ottobre 2011.
+</dd>
+
+<dt>Iscrizione</dt>
+<dd>
+<p>L'incontro &egrave; completamente libero, gratuito e aperto a tutti.
+</dd>
+
+<dt>Programma</dt>
+<dd>
+<p>Dice che senso ha preoccuparsi ora del prossimo Linux Day? La macchina organizzativa,
+che non muove n&eacute; riceve danari e che si base totalmente sul contributo volontario
+dei singoli in termini di tempo/impegno, necessita di tempi piuttosto lunghi.
+Il Linux Day &egrave; come il salame: si mangia in un attimo, ma la stagionatura richiede
+mesi.
+</dd>
+
+<dt>Contatti</dt>
+<dd>
+<p>Per qualsiasi richiesta di informazioni si pu&ograve; contattare l'organizzazione scrivendo all'indirizzo
+<a href="mailto:linuxday@lugbs.linux.it">linuxday@lugbs.linux.it</a>, oppure telefonando ad
+<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a> al numero 328/7296628.
+</dd>
+
+<dt>Aggiornamenti</dt>
+<dd>
+<p>Per restare aggiornati sull'organizzazione dell'evento &egrave; sufficiente mandare una mail a
+<a href="mailto:fatemisapere@lugbs.linux.it">fatemisapere@lugbs.linux.it</a>.
+</dd>
+
+<dt>Per gli smanettoni in ascolto...</dt>
+<dd>
+<p>Una mano in pi&ugrave; e nel LugBS, e durante il Linux Day, &egrave; sempre gradita.<br>
+&Egrave; sufficiente la voglia di sbattersi un po', non servono incredibili competenze
+tecnologiche, per offrire un aiuto ai curiosi e ai neofiti che si presentano durante
+le manifestazioni pubbliche.<br>
+Se hai voglia di donare un po' del tuo tempo, scrivi a
+<a href="mailto:sbattiamoci@lugbs.linux.it">sbattiamoci@lugbs.linux.it</a>.<br>
+Saremo felici di conoscerti!
+</dd>
+</dl>
+<?php include("../../../includes/footer.inc"); ?>
