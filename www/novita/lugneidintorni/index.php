b9d8d3a9a03ae9a6da1e2caa0be5ad5991548897
Andrea Gelmini <andrea.gelmini@gelma.net>
2012-04-20 21:09:28 +0200

    Rimossi i Lug non piu' esistenti

--- www/novita/lugneidintorni/index.php
+++ www/novita/lugneidintorni/index.php
@@ -5,8 +5,6 @@
 <ul>
  <li><a hreF="http://www.kenparker.eu/LugCastegnato/">Lug Castegnato</a> - Linux User Group di Castegnato
  <li><a href="http://www.lugob.org/">LugOB</a> - Linux User Group Ovest Bresciano
- <li><a href="http://www.gardalug.linux.it/">Garda Lug</a> - Garda Linux User Group
  <li><a href="http://www.bglug.it/">BGLug</a> - Bergamo Linux User Group
- <li><a href="http://lug.yoda2000.net/">Lug Universit&agrave; di Brescia</a> - Linux User Group Universit&agrave; di Brescia
 </ul>
 <?php include($_SERVER["DOCUMENT_ROOT"].'/includes/footer.inc'); ?>
