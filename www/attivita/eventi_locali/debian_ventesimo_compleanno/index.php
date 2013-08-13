8b09cbaf335a4418685c262be19279110abe982d
Andrea Gelmini <andrea.gelmini@gelma.net>
2013-08-13 21:39:10 +0200

    Sito: aggiunto ventesimo compleanno Debian

---
+++ www/attivita/eventi_locali/debian_ventesimo_compleanno/index.php
@@ -0,0 +1,42 @@
+<?php   $titolo='LugBS &raquo; Attivit&agrave; &raquo; Eventi Locali &raquo; Debian diventa ventenne';
+        include($_SERVER["DOCUMENT_ROOT"].'/includes/header.inc');
+?>
+
+<H3 align="center">Debian diventa ventenne</H3>
+
+<dt>Como?</dt>
+<dd>
+In occasione del <a href="http://www.debian.org/News/2013/20130809.it.html">ventesimo compleanno di Debian</a>
+ci troviamo a brindare. Ogni scusa &egrave; sempre buona per bere.
+</dd>
+
+<dt>Quando</dt>
+<dd>
+Venerd&igrave; 16 agosto 2013, dalle 18:30 in poi, saremo gentilmente ospitati dagli amici dello <a href="http://xander-beer.it/">Xander BrewPub</a>.
+</dd>
+
+<dt>Come arrivare</dt>
+La <a href="http://xander-beer.it/">birreria</a> si trova in <a href="http://xander-beer.it/dove-siamo/">via Dalmazia, 23/a</a>.
+Ovviamente a Brescia. Se hai bisogno di informazioni o ti sei perso puoi scrivere ad
+<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a>, oppure chiamarlo +39-328-7296628.
+Se hai bisogno di un passaggio manda una mail <a href="mailto:lug@lugbs.linux.it">in lista</a>. Probabile che qualcuno possa
+darti uno strappo.
+<dd>
+</dd>
+
+<dt>Partecipazione</dt>
+<dd>
+L'incontro &egrave; completamente libero, gratuito e aperto a tutti. Sul posto ci riconosci perch&eacute; siamo quelli sfigati in un angolo...
+</dd>
+
+<dt>Programma</dt>
+Chiacchiere e stupidaggini. &Egrave; una festa di compleanno. Non facciamo i nerd.
+</dd>
+
+<dt>Aggiornamenti</dt>
+<dd>
+Oltre alla gi&agrave; citata lista abbiamo anche un prestigioso <a href="http://www.facebook.com/">evento Facebook</a>.
+</dd>
+
+</dl>
+<?php include("../../../includes/footer.inc"); ?>
