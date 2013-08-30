81423ef6bac237931c8dc798638b784ebd57b53c
Andrea Gelmini <andrea.gelmini@gelma.net>
2013-08-30 22:39:23 +0200

    Aggiornamenti per il Linux Day 2013

--- www/attivita/linux_day/linux_day_17/index.php
+++ www/attivita/linux_day/linux_day_17/index.php
@@ -15,10 +15,11 @@ Nell'ambito della
 <a href="http://www.linuxday.it/">Giornata Nazionale di Linux e del Software Libero</a>,
 promossa da
 <a href="http://www.linux.it/">ILS</a>,
-il LugBS organizza il suo diciassettesimo Linux Day.
-La manifestazione ha luogo <b>Sabato 26 Ottobre 2013</b>. Dove - ancora - non si sa.
+il LugBS organizza il suo diciassettesimo Linux Day.<br>
+La manifestazione ha luogo <b>sabato 26 Ottobre 2012</b> presso il
+<a href="http://www.musilbrescia.it/minisiti/simil/web/musei-enti/brescia-provincia/sistema-musil/musil_rodengo_saiano/">Musil, Museo dell'industria e del lavoro</a>
+di Rodengo Saiano (per intenderci, basta parcheggiare all'<a href="http://goo.gl/maps/4VNga">Outlet di Franciacorta</a> e siete sul posto).
 </dd>
-
 
 <dd>
 L'incontro &egrave; completamente libero, gratuito e aperto a tutti.
@@ -26,7 +27,26 @@ L'incontro &egrave; completamente libero, gratuito e aperto a tutti.
 
 <dt>Programma</dt>
 <dd>
-In via di definizione.
+La giornata si articola in due momenti:
+<ul>
+<li>lo spazio mattutino, dalle 10 alle 12: sessione riservata ai talk. La scaletta si compone dei seguenti interventi:
+·⁖⁖⁖<ul>
+·⁖⁖⁖<li>h.10:00 (<a href="mailto:luisa.ravelli@lugbs.linux.it">Luisa Ravelli</a>): benvenuto e presentazione della giornata;
+·⁖⁖⁖<li>h.10:10 (<a href="mailto:alessandro.ferrari@lugbs.linux.it">ing. Alessandro Ferrari</a>): <a href="https://it.wikipedia.org/wiki/Software_libero">strumenti liberi</a> per l'analisi delle immagini;
+·⁖⁖⁖<li>h.10:30 (<a href="mailto:mattia.pescimoro@lugbs.linux.it">Mattia Pescimoro</a>):
+·⁖⁖⁖<li>h.10:50 (<a href="mailto:francesco.abeni@lugbs.linux.it">Francesco Abeni</a>): titolare di <a href="http://www.gibilogic.com/">GiBiLogic</a>.
+·⁖⁖⁖<li>h.11:10 pausa caff&egrave;
+·⁖⁖⁖<li>h.11:30
+·⁖⁖⁖<li>h 11:50
+·⁖⁖⁖</ul>
+<p>A causa del limitato numero di posti &egrave; cortesemente richiesta, per la partecipazione mattutina, una segnalazione in via elettronica (scrivendo una email a <a href="mailto:linuxday@lugbs.linux.it">linuxday@lugbs.linux.it</a>, o telefonica chiamando <a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a> al 328/7296628.
+
+<li>lo spazio pomeridiano, dalle 14 alle 18: stand monotematici ove chiedere informazioni e
+provare con mano i software presentati sulle postazioni liberamente utilizzabili.
+&Egrave; inoltre possibile portare il proprio PC per essere aiutati dai membri del LUG
+nell'installazione di Linux, o nella risoluzione di problemi incontrati.
+</ul>
+
 </dd>
 
 <dt>Contatti</dt>
