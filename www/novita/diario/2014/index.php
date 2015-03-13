93a1a75b546bb1ce274a91e6cad5898fe4d6191a
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-03-13 10:39:28 +0100

    Portato anno 2014

---
+++ www/novita/diario/2014/index.php
@@ -0,0 +1,27 @@
+<?php
+//depends_on: log_eventi.txt
+include("../../../includes/header.inc");
+
+print"<H3 align=center>Diario del LugBS - 2014</H3>";
+print'<TABLE align=center cellpadding=5 cellspacing=0 width="85%">';
+        $file = 'log_eventi.txt';
+        $feventi = file($file); 
+        while (list ($riga,$evento) = each ($feventi)) {
+                $campi = explode("|",$evento);
+                print"<TR>
+                       <TD valign=top>
+                        <B>".ereg_replace(' ','&nbsp;',$campi[0])."</B>
+                       </TD>
+                       <TD>
+                        $campi[1]
+                       </TD>
+                      </TR>
+                       <TR>
+                        <TD colspan=2>
+                         <HR>
+                        </TD>
+                       </TR>";
+                                                        }
+print '</TABLE>';
+include("../../../includes/footer.inc");
+?>
