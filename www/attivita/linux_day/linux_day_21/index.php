8ee4a25ca5ca3d3656e074e3ed98f122107dcb2b
Andrea Gelmini <andrea.gelmini@gelma.net>
2017-10-03 21:03:56 +0200

    Aggiunto Linux Day 2017

---
+++ www/attivita/linux_day/linux_day_21/index.php
@@ -0,0 +1,117 @@
+<?php   $titolo='LugBS &raquo; Attivit&agrave; &raquo; Linux Day &raquo; Ventesima edizione';
+        include($_SERVER["DOCUMENT_ROOT"].'/includes/header.inc');
+?>
+
+<H3 align="center">Ventesimo Linux Day: I fatti nostri!</H3>
+
+<dt>Data e Luogo</dt>
+<dd>
+Nell'ambito della
+<a href="http://www.linuxday.it/">Giornata Nazionale di Linux e del Software Libero</a>,
+promossa da
+<a href="http://www.linux.it/">ILS</a>,
+il LugBS organizza il suo ventunesimo Linux Day.<br>
+La manifestazione ha luogo <b>sabato 28 Ottobre 2017</b> presso il
+<a href="http://www.musilbrescia.it/minisiti/simil/web/musei-enti/brescia-provincia/sistema-musil/musil_rodengo_saiano/">Musil, Museo dell'industria e del lavoro</a>
+di Rodengo Saiano (per intenderci, basta parcheggiare all'<a href="http://goo.gl/maps/4VNga">Outlet di Franciacorta</a> e siete sul posto).<br />
+</dd>
+<dd>
+L'incontro &egrave; completamente libero, gratuito e aperto a tutti.
+<!--Sono disponibili il <a href="VolantinoLinuxDay2015.pdf">volantino della giornata</a> e il <a href="VolantinoLinuxDay2015talk.pdf">volantino della mattinata</a>.-->
+</dd>
+
+<dd>
+<iframe
+    width="50%"
+    height="300px"
+    frameBorder="0"
+    src="http://umap.openstreetmap.fr/en/map/untitled-map_18981?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined">
+</iframe>
+<p><a href="http://umap.openstreetmap.fr/en/map/untitled-map_18981">Mappa ingrandita</a></p>
+</dd>
+
+<dt>Programma</dt>
+<dd>
+
+La giornata si articola in tre momenti:
+<ul>
+<li>lo spazio mattutino, dalle 10 alle 12.30, riservato ai talk. In particolare, quest'anno, il tema riguarda
+    la privacy e la gestione delle proprie informazioni. Ci focalizzeremo quindi sulle soluzioni libere per
+    avere cura dei nostri dati.
+   <!--ul>
+   <li>h.10:00 (<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Benvenuto e presentazione della giornata</b><hr>
+   <li>h.10:05 (<a href="mailto:marco.manenti@lugbs.linux.it">dott. Marco Manenti</a> e <a href="mailto:robotica@dreampuzzle.net">Silvia Cerioni</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>La lingua delle cose: giochiamo a programmare. Esperienze di programmazione nell'infanzia.</b><hr>
+   <li>h.10:15 (<a href="mailto:giorgio.pasiniruffoni@lugbs.linux.it">ing. Giorgio Pasini Ruffoni</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Bitcoin e gestionali liberi: un felice plugin.</b><hr>
+   <li>h.10:25 (<a href="mailto:alessandro.ferrari@lugbs.linux.it">ing. Alessandro Ferrari</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Il computer come Van Gogh: la creativit&agrave; nella computer vision.</b><hr>
+   <li>h.10:40 (<a href="mailto:mattia.rizzolo@lugbs.linux.it">Mattia Rizzolo</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>I nuovi Gutenberg: l'esperienza del gruppo di <a href="http://wiki.ubuntu-it.org/Fcm/">Full Circle Magazine Italia</a>.</b><hr>
+   <li>h.10:55 (<a href="mailto:alberto.bontempi@lugbs.linux.it">Alberto Bontempi</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Stormageddon: storia di un amore tra i rifiuti digitali.</b><hr>
+   <li>h.11:10<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Pausa caff&egrave;</b><hr>
+   <li>h.11:25 (<a href="mailto:michele.faini@lugbs.linux.it">Michele Faini</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Il FabLab Brescia e la stampante 3D di cioccolata.</b><hr>
+   <li>h.11:40 (<a href="mailto:daniele.barattieri@lugbs.linux.it">Daniele Barattieri</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Protesi digitali e Software Libero.</b><hr>
+   <li>h.11:55 (<a href="mailto:giovanni.turra@lugbs.linux.it">ing. Giovanni Turra</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Moderni Yoda: imparare online.</b><hr>
+   <li>h.12:10 (<a href="mailto:mattia.pescimoro@lugbs.linux.it">Mattia Pescimoro</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href="http://www.muhack.org/">µhack</a> presenta: <i>Hack 'n Smash</i>.</b><hr>
+   <li>h.12:20 (<a href="mailto:matteo.battaglio@lugbs.linux.it">ing. Matteo Battaglio</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Strumenti liberi per teste chiuse!</b><hr>
+   <li>h.12:30 (<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a>)<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>LibreOffice: siamo tutti sviluppatori!</b><hr>
+   <li>h.12:40<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Saluti</b><hr>
+   </ul-->
+<p>Per poter offrire uno spazio confortevole &egrave; cortesemente richiesta,
+per la partecipazione mattutina, una segnalazione in via elettronica
+(scrivendo una email a
+<a href="mailto:linuxday@lugbs.linux.it">linuxday@lugbs.linux.it</a>, o
+telefonica chiamando
+<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a> al 328/7296628.
+
+<li>lo spazio pomeridiano, <b>dalle 14 alle 18</b>: stand monotematici ove
+chiedere informazioni e provare con mano le soluzioni hardware (come stampanti 3D,
+robot e droni), i software presentati (programmi didattici, soluzioni per
+le scuole, il multimediale, i giochi, i servizi, ecc.), il tutto sulle postazioni
+liberamente disponibili e utilizzabili.
+&Egrave; inoltre possibile portare il proprio PC per essere aiutati dai
+membri del LUG nell'installazione di Linux, o nella risoluzione di eventuali
+problemi riscontrati.<br>
+
+<!--<p><li>lo spazio Hack 'n Smash, <b>dalle 15 alle 18</b>. Per scoprire di cosa si tratta
+ti invitiamo a <a href="http://www.muhack.org/eventi/2-hack-n-smash">leggere qui</a>, se sei interessato a partecipare
+prendi visione del regolamento <a href="Regolamento_HackNSmash.pdf">qui</a>. Per l'iscrizione gratuita, da effettuarsi entro il 17 ottobre, puoi usare il <a href="Iscrizione_HackNSmash.pdf">modulo di iscrizione</a>.
+Per tutte le informazioni aggiornate potete seguire l'<a href="https://www.facebook.com/events/376879079179943/">evento su Facebook</a>.
+L'evento &egrave; reso possibile grazie allo sforzo dei ragazzi del
+<a href="http://www.muhack.org/">µhack</a>.-->
+</ul>
+
+</dd>
+
+<dt>Contatti</dt>
+<dd>
+Per qualsiasi richiesta di informazioni si pu&ograve; contattare l'organizzazione scrivendo all'indirizzo
+<a href="mailto:linuxday@lugbs.linux.it">linuxday@lugbs.linux.it</a>, oppure telefonando ad
+<a href="mailto:andrea.gelmini@lugbs.linux.it">Andrea Gelmini</a> al numero 328/7296628.
+</dd>
+
+<dt>Aggiornamenti</dt>
+<dd>
+Per restare aggiornati sull'organizzazione dell'evento &egrave; sufficiente mandare una mail a
+<a href="mailto:fatemisapere@lugbs.linux.it">fatemisapere@lugbs.linux.it</a>, oppure seguirne
+la relativa <a href="https://www.facebook.com/groups/lugbs/">pagina Facebook</a>.
+</dd>
+
+<dt>Per gli smanettoni in ascolto&#8230;</dt>
+<dd>
+Una mano in pi&ugrave; e nel LugBS, e durante il Linux Day, &egrave; sempre gradita.<br>
+&Egrave; sufficiente la voglia di sbattersi un po', non servono incredibili competenze
+tecnologiche, per offrire un aiuto ai curiosi e ai neofiti che si presentano durante
+le manifestazioni pubbliche, o anche solo per aiutarci nell'allestimento/logistica.<br>
+Se hai voglia di donare un po' del tuo tempo, scrivi a
+<a href="mailto:sbattiamoci@lugbs.linux.it">sbattiamoci@lugbs.linux.it</a>.<br>
+Saremo felici di conoscerti!
+</dd>
+
+<dt>Nei dintorni&#8230;</dt>
+<dd>
+Nella stessa giornata si svolgono <a href="http://www.linuxday.it/">Linux Day in tutta l'Italia</a>.
+<!--In particolare nella nostra provincia sono da segnalare gli eventi di
+<a href="http://lugotto.linux.it/categoria/66/Linux-Day-2015-Ist.-V.-Capirola-Leno-%28Brescia%29.html">Gottolengo</a> e
+<a href="http://www.kenparker.eu/LugCastegnato/viewtopic.php?id=786">Castegnato</a>.-->
+</dd>
+
+</dl>
+<?php include("../../../includes/footer.inc"); ?>
